9f409219fdcd8c8af4340ac4cec5c525ff9f33f9
Jack <fighterbee87@gmail.com>
2021-05-04 15:38:00 -0700

    search keyword available

--- routes/search.js
+++ routes/search.js
@@ -7,7 +7,7 @@ const { csrfProtection, asyncHandler } = require("./utils");
 const { loginUser, logoutUser } = require("../auth");
 const { check, validationResult } = require("express-validator");
 
-
+const { Op } = require("sequelize");
 
 router.get('/',csrfProtection,asyncHandler(async(req,res)=>{
     const Tags=await Tag.findAll();
@@ -18,7 +18,7 @@ router.get('/',csrfProtection,asyncHandler(async(req,res)=>{
 
 }));
 
-router.post('/tag',csrfProtection,asyncHandler(async(req,res)=>{
+router.post('/tags',csrfProtection,asyncHandler(async(req,res)=>{
     const result=req.body
     let id=[];
     let questions=[]
@@ -26,10 +26,11 @@ router.post('/tag',csrfProtection,asyncHandler(async(req,res)=>{
     for(let i=0;i<vals.length-1;i++){
         id.push(parseInt(vals[i]))
     }
-    // console.log(id)
+    console.log(id)
     let tags=await Tag.findAll({
         where:{id:[...id]},
         include:Question
+        
     })
     for(let i=0;i<tags.length;i++){
         questions.push(tags[i].Questions[0].content)
@@ -37,13 +38,24 @@ router.post('/tag',csrfProtection,asyncHandler(async(req,res)=>{
     // console.log(tags[0].Questions[0])
     res.render('search-result',{questions})
 }));
-router.post("/keyword",csrfProtection,asyncHandler(async (req, res) => {
 
+router.post('/keyword',csrfProtection,asyncHandler(async(req,res)=>{
+    const {searchBar}=req.body
+    const results=await Question.findAll({
+        where:{
+            content:{
+                [Op.substring]:searchBar
+            }
+        }
+    })
+    let questions=[];
+    for(let i=0;i<results.length;i++){
+        questions.push(results[i].content)
+    }
+    // console.log(results[0].content)
+    res.render("search-result", { questions });
+}))
 
-
-
-
-}));
 
 
 
